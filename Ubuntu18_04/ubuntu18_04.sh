7e66c66b752328fbf3d325c550c353d7409192b4
Balu M N <balumn@ieee.org>
2019-12-12 10:47:43 +0530

    added anydesk, mysql-workbench; commented android-studio

--- Ubuntu18_04/ubuntu18_04.sh
+++ Ubuntu18_04/ubuntu18_04.sh
@@ -3,12 +3,12 @@ apt update -y && apt upgrade -y
 add-apt-repository universe -y
 
 # essential tools
-apt install -y gnome-tweak-tool gcc dkms build-essential vim curl make guake terminator python3-setuptools python3-pymysql python3-pip python3-venv git fish tee neofetch
+apt install -y gnome-tweak-tool gcc dkms build-essential vim curl make guake terminator python3-setuptools python3-pymysql python3-pip python3-venv git fish tee neofetch mysql-workbench
 snap install vlc
 snap remove vscode
 snap install code --classic
 snap install spotify
-snap install android-studio --classic
+#snap install android-studio --classic
 snap install slack
 snap install brave
 snap install robo3t-snap
@@ -27,11 +27,18 @@ apt-add-repository -y ppa:teejee2008/ppa
 apt-get update
 apt-get install -y timeshift  
 
-# installing google chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-dpkg -i google-chrome-stable_current_amd64.deb
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb # downloading google chrome
+wget https://download.anydesk.com/linux/anydesk_5.5.1-1_amd64.deb # downloading anydesk
+
+dpkg -i *.deb
 
 # installing pop os theme
 add-apt-repository -y ppa:system76/pop
 apt install pop-theme
+
+
+
+# setup finished - clearing caches and rebooting
 apt autoremove 
+reboot
